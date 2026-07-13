01de414eeb5448df4db90d5be3f1b4e893147186
Bobby Pearson <pearson.bobby@gmail.com>
2026-07-13 15:56:10 +0100

    Renamed task checkbox
    Effectively reverts the earlier change, renaming the checkbox testid
    back to what it was originally

--- functional-tests/model/pages/task-manager.ts
+++ functional-tests/model/pages/task-manager.ts
@@ -49,7 +49,7 @@ export class TaskManagerPage extends BasePage {
   }
 
   taskCheckbox(title: string): Locator {
-    return this.taskItem(title).locator('[data-testid^="task-ckbx-"]')
+    return this.taskItem(title).locator('[data-testid^="task-checkbox-"]')
   }
 
   taskDeleteButton(title: string): Locator {
